4b8701c086cfa9bd5d4098ee66c7ecb8f4161dcf
cdriesler <cdriesler.iv@gmail.com>
2018-11-03 21:34:31 -0400

    implement wpf window from grasshopper component

---
+++ Resthopper.Core/ViewModels/MainPanelViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.CommandWpf;
+using Rhino;
+
+namespace Resthopper.Core.ViewModels
+{
+    internal class MainPanelViewModel : Rhino.UI.ViewModel
+    {
+        public RelayCommand DeclareInputsCommand { get; set; }
+
+        public MainPanelViewModel()
+        {
+            DeclareInputsCommand = new RelayCommand(OnDeclareInputs);
+        }
+
+        public void OnDeclareInputs()
+        {
+            RhinoApp.WriteLine("Good!");
+        }
+    }
+}
---
+++ Resthopper.Core/Views/MainPanelView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Resthopper.Core.ViewModels;
+
+namespace Resthopper.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for MainPanelView.xaml
+    /// </summary>
+    public partial class MainPanelView
+    {
+        public MainPanelView()
+        {
+            DataContext = new MainPanelViewModel();
+            InitializeComponent();
+        }
+
+        private MainPanelViewModel ViewModel => DataContext as MainPanelViewModel;
+    }
+}
